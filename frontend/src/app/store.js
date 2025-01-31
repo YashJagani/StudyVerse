a7dbdc00b13a13cbf55181da13f985d3f3caf52c
YashJagani <97232532+YashJagani@users.noreply.github.com>
2025-01-30 20:05:37 -0500

    Delete frontend directory

--- frontend/src/app/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import rootReducer from "./rootReducer"; 
-import { authApi } from "@/features/api/authApi.js";
-
-export const appStore = configureStore({
-  reducer: rootReducer,
-  middleware: (defaultMiddleware) =>
-    defaultMiddleware().concat(authApi.middleware),
-});
